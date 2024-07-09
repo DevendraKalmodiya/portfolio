a907e7c9772da984dfa208502ec3b93bce1670d5
DevendraKalmodiya <devendrakalmodiya1@gmail.com>
2024-07-09 21:55:03 +0530

    Add files via upload

---
+++ Devendra's portfolio/main.js
@@ -0,0 +1,9 @@
+var typed=new Typed(".textxyz",{
+   strings: ["Full Stack Developer","Java Developer","Web Developer"],
+    typeSpeed:50,
+    backSpeed:50,
+    backDelay:500,
+    loop:true
+}
+);
+
